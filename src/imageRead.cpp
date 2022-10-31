49f74e90eaa1b260f6b7888cf8d761cf1de1da86
Eduard Frigola Bagué <eduardfrigola@gmail.com>
2022-10-31 17:52:09 +0100

    [Image Read] - update reading interpolation

--- src/imageRead.cpp
+++ src/imageRead.cpp
@@ -16,23 +16,26 @@ imageRead::imageRead(){
 void imageRead::update(Image image, int size, int position){
     points[0].x = position;
     points[0].y = 0;
-    for (int i = 0; i < image.height; i++)
+    for (int i = 0; i < size; i++)
     {
-        //BGR& bgr = source.ptr<BGR>(i)[position];
-        //vals[i] = (0.299 * bgr.red/255.0f + 0.587 * bgr.green/255.0f + 0.114 * bgr.blue/255.0f);
-        Color color = GetImageColor(image, position, i);
-        readVals[i] = (0.299 * color.r/255.0f + 0.587 * color.g/255.0f + 0.114 * color.b/255.0f);
+	int index = (((float)i / (float)size) * image.height);
+	float tempSum = 0;
+	int steps = 0;
+	for(int j = floor(index); j < floor(index + (image.height/size)); j++){
+		Color color = GetImageColor(image, position, j);
+		tempSum += (0.299 * color.r/255.0f + 0.587 * color.g/255.0f + 0.114 * color.b/255.0f);
+		steps++;
+	}
+	readVals[i] = tempSum/steps;
+	vals[i] = readVals[i];
         points[i+1].x = position - (readVals[i] * 50); //Graphic scaling
-        //TODO: review line draw
         points[i+1].y = ((float)(i+1) / (float)(size+1)) * 720;
+    }
+    for (int i = size; i < 360; i++){
+	vals[i] = 0;
     }
     points[size+1].x = position;
     points[size+1].y = 720;
-
-
-    for(int i = 0; i < size; i++){
-        vals[i] = readVals[i*2] + readVals[(i*2) + 1];    
-    }
 }
 
 void imageRead::draw(int size){
